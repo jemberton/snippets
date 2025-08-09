de824f003cdb3353d3eb3ba482d94bc8e86c3818
Josh Emberton <josh.emberton@gmail.com>
2025-08-09 17:23:50 -0400

    updated snippets.sh to remove current css and replace with project css

--- snippets.sh
+++ snippets.sh
@@ -2,44 +2,18 @@
 
 clear
 echo "----------------------------------------"
-echo "Obsidian CSS Snippets Picker"
+echo "Obsidian CSS Snippets Tool"
 echo "----------------------------------------"
 
-options=("Exit" "universal" "current" "Create New")
+VAULT=/tank/obsidian-vault/private/.obsidian/snippets
+SCRIPT=$(echo "$PWD")
 
-while true; do
-    PS3="Select project: "
-    
-    select opt in "${options[@]}"; do
-        case $opt in
-            "Create New")
-                echo "$opt"
-                echo "[dev] New project stuff ..."
-                read -p "Project Name: " project_name
-                mkdir "$project_name"
-                echo "[dev] would you like to select this repo?"
-                exit 0
-                ;;
-            "universal")
-                echo "$opt"
-                echo "[dev] universal repo ..."
-                exit 0
-                ;;
-            "current")
-                echo "$opt"
-                echo "[dev] current repo ..."
-                exit 0
-                ;;
-            "Exit")
-                echo "$opt ..."
-                echo "Bye."
-                exit 0
-                ;;
-            *)
-                echo ""
-                echo "Invalid option."
-                ;;
-        esac
-    done
-done
+echo "Removing current stylesheets from Obsidian vault ..."
+rm "$VAULT"/*.css
+
+echo "Copying universal stylesheets to Obsidian vault ..."
+cp ./css/*.css "$VAULT"
+
+echo "That's it! Update your settings in the Appearance section of your vault settings."
+exit 0
 
