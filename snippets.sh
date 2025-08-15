61f03eda2cd6b39f48fbb60dabd02ed6388af458
Josh Emberton <josh.emberton@gmail.com>
2025-08-14 21:29:11 -0400

    added copying and updating of test note to snippets.sh

--- snippets.sh
+++ snippets.sh
@@ -5,26 +5,30 @@ echo "----------------------------------------"
 echo "Obsidian CSS Snippets Tool"
 echo "----------------------------------------"
 
-VAULT=/tank/obsidian-vault/private/.obsidian/snippets
+VAULT=/tank/obsidian-vault/private
+SNIPPETS=.obsidian/snippets
 SCRIPT=$(echo "$PWD")
 
 if [[ "$1" == "copy" ]]; then
     echo "Removing current stylesheets from Obsidian vault ..."
-    rm "$VAULT"/*.css
+    rm "$VAULT/$SNIPPETS"/*.css
 
     echo "Copying universal stylesheets to Obsidian vault ..."
-    cp ./css/*.css "$VAULT"
+    cp ./css/*.css "$VAULT/$SNIPPETS"
+    # echo "Copying test note to Obsidian Vault ..."
+    # cp ./Obsidian\ Test\ Note.md "$VAULT"
     
     echo "That's it! Update your settings in the Appearance section of your vault settings."
     exit 0
 elif [[ "$1" == "update" ]]; then
     echo "Copying stylesheets from Obsidian vault to project ..."
-    cp "$VAULT"/*.css ./css
-    
+    cp "$VAULT/$SNIPPETS"/*.css ./css
+    # echo "Copying test note from Obsidian vault to project ..."
+    # cp "$VAULT/Obsidian\ Test\ Note.md" .
+
     echo "You should update the git repository."
     exit 0
 else
     echo "Doing nothing ... (try 'copy' or 'update')"
     exit 0
 fi
-
