981fa7f800b18a46fb8db1aef02963ac52e5512c
Josh Emberton <josh.emberton@gmail.com>
2025-09-27 12:07:10 -0400

    updated the user script to include test note asset copying and configuration

--- README.md
+++ README.md
@@ -2,7 +2,20 @@
 
 A set of automation tools for Obsidian snippet switching.
 
-## universal
+## Branches
+
+### main
+
+### stable
+
+### universal
 
 This branch of the repository contains CSS stylesheets that *should* be universal with any theme. It does not attempt to be a theme on its own but does have some elements that set styles. This integration is meant to work using the Obsidian namespace for CSS, so it should be widely universal.
 
+***
+
+## CSS Snippets
+
+## Markdown
+
+## Assets
--- obsidian-snippets.sh
+++ obsidian-snippets.sh
@@ -1,5 +1,7 @@
 #! /usr/bin/env bash
 
+COPY_ASSETS=0
+
 echo "----------------------------------------"
 echo "Obsidian CSS Snippets Tool v1.0"
 echo "----------------------------------------"
@@ -14,25 +16,42 @@ if [ ! -f .env ]; then
     while read -p "[snippets] Enter path to Obsidian vault: " OBSIDIAN_VAULT_PATH; do
         if [ ! -z "$OBSIDIAN_VAULT_PATH" ]; then
             if [ -d "$OBSIDIAN_VAULT_PATH" ]; then
-                echo "[snippets] Creating environment file ..."
-                echo "OBSIDIAN_VAULT_PATH=$OBSIDIAN_VAULT_PATH" > .env
                 break
             else
                 echo "[snippets] ERROR: $OBSIDIAN_VAULT_PATH is not a valid path."
             fi
         fi
     done
+    while read -p "[snippets] Enter folder name for images: " OBSIDIAN_VAULT_IMAGES; do
+        if [ ! -z "$OBSIDIAN_VAULT_IMAGES" ]; then
+            if [ -d "$OBSIDIAN_VAULT_PATH/$OBSDIAN_VAULT_IMAGES" ]; then
+                break
+            else
+                echo "[snippets] ERROR: $OBSIDIAN_VAULT_IMAGES is not a valid path."
+            fi
+        fi
+    done
+    echo "[snippets] Creating environment file ..."
+    echo "OBSIDIAN_VAULT_PATH=$OBSIDIAN_VAULT_PATH" > .env
+    echo "OBSIDIAN_VAULT_IMAGES=$OBSIDIAN_VAULT_IMAGES" >> .env
     echo "----------------------------------------"
 fi
 
 # Initialize the .env file
 source .env
 echo "[snippets] Using vault at '$OBSIDIAN_VAULT_PATH'"
+echo "[snippets] Using images/assets at '$OBSIDIAN_VAULT_IMAGES'"
 
 if [ ! -d "$OBSIDIAN_VAULT_PATH"/.obsidian/snippets ]; then
     echo "[snippets] ERROR: Cannot find the snippets folder!"
     echo "[snippets] ERROR: The vault path doesn't seem to be a valid Obsidian vault."
     exit 1
+fi
+
+if [ ! -d "$OBSIDIAN_VAULT_PATH/$OBSIDIAN_VAULT_IMAGES" ]; then
+    echo "[snippets] WARNING: Cannot find the images/assets folder!"
+else
+    COPY_ASSETS=1
 fi
 
 echo "[snippets] Removing old CSS snippets from vault ..."
@@ -43,5 +62,10 @@ cp ./css/*.css "$OBSIDIAN_VAULT_PATH"/.obsidian/snippets > /dev/null 2>&1 || ech
 
 echo "[snippets] Copying repository test note to vault ..."
 cp ./Obsidian\ Test\ Note.md "$OBSIDIAN_VAULT_PATH" > /dev/null 2>&1 || echo "[snippets] ERROR: Could not copy the test note to the vault."
+
+if [ "$COPY_ASSETS" -eq 1 ]; then
+    echo "[snippets] Copying test note assets to vault ..."
+    cp -R ./_assets/* "$OBSIDIAN_VAULT_PATH/$OBSIDIAN_VAULT_IMAGES" > /dev/null 2>&1 || echo "[snippets] ERROR: Could not copy the test note assets to the vault."
+fi
 
 echo "[snippets] Enable Obsidian snippets in Settings > Appearance"
